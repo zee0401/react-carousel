cde2ba5cd3b75391a30116324a347d1b7336cceb
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-06 15:21:17 +0530

    next button added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,27 +6,49 @@ const Carousel = ({ children }) => {
 
   const slideCurrentRef = useRef();
 
+  const intervalRef = useRef(0);
+
+  function handleInterval() {
+    const slideRef = slideCurrentRef.current;
+    const slides = slideRef.children;
+    slides[0].setAttribute("data-active", true);
+    intervalRef.current = setInterval(() => {
+      setCurrentSlide((prev) => {
+        const count = slides.length;
+        const newIndex = prev === count - 1 ? 0 : prev + 1;
+        [...slides].forEach((slide, index) => {
+          slide.setAttribute("data-active", index === newIndex);
+        });
+        return newIndex;
+      });
+    }, 2000);
+  }
+
   useEffect(() => {
     const slideRef = slideCurrentRef.current;
     const slides = slideRef.children;
     slides[0].setAttribute("data-active", true);
 
-    setInterval(() => {
-      setCurrentSlide((prev) => {
-        // slideRef.style.opacity = 1;
-
-        const count = slides.length;
-
-        const newIndex = prev === count - 1 ? 0 : prev + 1;
-
-        [...slides].forEach((slide, index) => {
-          slide.setAttribute("data-active", index === newIndex);
-        });
-
-        return newIndex;
-      });
-    }, 1000);
+    handleInterval();
   }, []);
+
+  function handlePrev() {}
+
+  function handleNext() {
+    clearInterval(intervalRef.current);
+    const slideRef = slideCurrentRef.current;
+    const slides = slideRef.children;
+    const count = slides.length;
+
+    const newIndex = currentSlide === count - 1 ? 0 : currentSlide + 1;
+
+    [...slides].forEach((slide, index) => {
+      slide.setAttribute("data-active", index === newIndex);
+    });
+    handleInterval();
+
+    setCurrentSlide(newIndex);
+  }
 
   return (
     <div className="carousel">
@@ -34,6 +56,9 @@ const Carousel = ({ children }) => {
       <div ref={slideCurrentRef} className="box">
         {children}
       </div>
+
+      <button onClick={handlePrev}>Prev</button>
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 };
