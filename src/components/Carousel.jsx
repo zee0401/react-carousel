3557463ef3e61b38c5726e5b75cad1c5446dd4e1
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-06 12:44:24 +0530

    ref added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react";
 
 const Carousel = ({ children }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slideCurrentRef = useRef();
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentSlide((prev) => {
+        // slideRef.style.opacity = 1;
+        const slideRef = slideCurrentRef.current;
+        console.log(slideRef.children);
+        const count = slideRef.children.length;
+
+        const newIndex = prev === count ? 0 : prev + 1;
+        return newIndex;
+      });
+    }, 1000);
+  }, []);
+
   return (
     <div className="carousel">
-      <div className="box">{children}</div>
+      {currentSlide}
+      <div ref={slideCurrentRef} className="box">
+        {children}
+      </div>
     </div>
   );
 };
